2533ecd1cf22178b5bccfa6f2213fcf692b77482
pankajpandeyiwtechnocrats-pixel <pankajpandeyiwtechnocrats@gmail.com>
2026-02-06 17:40:13 +0530

    Update API_URL to new backend endpoint

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -1,4 +1,4 @@
-const API_URL = "https://waba-backend-wq2d.onrender.com/api/templates";
+const API_URL = "https://waba-backend-2.onrender.com/api/templates";
 
 const token = () => localStorage.getItem("authToken");
 
